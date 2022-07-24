ae3d352d6bbe73318a5292e444e4db8bdc8888cb
deathsmilestome <vanekt35@mail.ru>
2022-07-24 23:52:29 +0300

    - media

--- firstTry/myProjectsapp/models.py
+++ firstTry/myProjectsapp/models.py
@@ -8,7 +8,6 @@ class Project(models.Model):
     title = models.CharField(max_length=500)
     lang = models.CharField(max_length=50)
     text = models.TextField(max_length=2000)
-    image = models.ImageField(upload_to='project_imgs/', null = True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
