fd461642d293f64275ddae1ffdd4e6150f33063d
deathsmilestome <vanekt35@mail.ru>
2022-08-07 16:58:27 +0300

    + POST method for projects

--- firstTry/api/urls.py
+++ firstTry/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import get_projects, get_projects_k, get_projects_p
+from .views import AllProjects, get_projects_k, get_projects_p
 
 urlpatterns = [
-    path('projectlist/', get_projects),
+    path('projectlist/', AllProjects.as_view()),
     path('projectlist/python', get_projects_p),
     path('projectlist/kotlin', get_projects_k),
 
--- firstTry/api/views.py
+++ firstTry/api/views.py
@@ -1,14 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import ProjectSerializer
-from myProjectsapp.models import Project
+from myProjectsapp.models import Project, Category
 
 
-@api_view(['GET'])
-def get_projects(request):
-    projects = Project.objects.all().order_by('-id')
-    serializer = ProjectSerializer(projects, many=True)
-    return Response(serializer.data)
+class AllProjects(APIView):
+    def get(self, request):
+        projects = Project.objects.all().order_by('-id')
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        npd = request.data
+        category_name = Category.objects.get(name=npd["category"])
+        new_project = Project.objects.create(category=category_name, name=npd["name"], title=npd["title"], path=npd["path"], text=npd["text"])
+        new_project.save()
+        serializer=ProjectSerializer(new_project)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
